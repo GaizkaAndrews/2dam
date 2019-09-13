e7cc1ade8847c19a5f44a1262e3e71987585d418
2dam <2dam@MACINF07.TartangaLH.eus>
2019-09-13 12:34:24 +0200

    HolaMundoConCapas version 1.0

---
+++ test/holamundoconcapas/model/ModelImplementationTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoconcapas.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 2dam
+ */
+public class ModelImplementationTest {
+    
+    public ModelImplementationTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getGreeting method, of class ModelImplementation.
+     */
+    @Test
+    public void testGetGreeting() {
+            
+    }
+    
+}
---
+++ test/holamundoconcapas/view/ViewFactoryTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundoconcapas.view;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 2dam
+ */
+public class ViewFactoryTest {
+    
+    public ViewFactoryTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getView method, of class ViewFactory.
+     */
+    @Test
+    public void testGetView() {
+        ViewFactory ViewFact = new ViewFactory();
+        assertNotNull(ViewFact.getView());
+        
+    }
+    
+}
